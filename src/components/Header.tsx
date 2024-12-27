ffacc700df0617aa88c75f7600383a3b5c925f23
BPS Kabupaten Siak <prasetyofmalik@gmail.com>
2024-12-27 09:51:45 +0700

    updated assets

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,7 @@ export const Header = () => {
     <header className="border-b bg-white">
       <div className="container mx-auto px-4 py-4 flex items-center justify-between">
         <div className="flex items-center space-x-2">
-          <img src="/img/bps-siak-logo.png" alt="SAMS Logo" className="h-8 w-auto" />
-          <span className="text-xl font-semibold text-primary">SAMS</span>
+          <img src="/img/sams-logo.png" alt="SAMS Logo" className="h-8 w-auto" />
         </div>
         <div className="flex items-center space-x-4">
           <Button variant="ghost" asChild>
